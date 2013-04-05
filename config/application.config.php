9a5e0110e125ac3227326d1a8d31b4a6b2c3ceef
Eric Paige <eric@ericpaige.com>
2013-04-05 00:30:51 +0000

    Added to the modules array

--- config/application.config.php
+++ config/application.config.php
@@ -3,6 +3,8 @@ return array(
     // This should be an array of module namespaces used in the application.
     'modules' => array(
         'Application',
+        'Album',                  // <-- Add this line. ericp
+        'MyAcl',                  // <-- Add this line. ericp
     ),
 
     // These are various options for the listeners attached to the ModuleManager
